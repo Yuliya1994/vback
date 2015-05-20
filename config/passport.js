80c9533b18b45243a9aad5254772ce6ad4bc7992
Yuliya1994 <yuliaharchenko1994@gmail.com>
2015-05-20 16:58:50 +0300

    add validation email

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,8 @@
 /*
-* Levels:
-* 0 - Admin
-* 1 - User
-* */
+ * Levels:
+ * 0 - Admin
+ * 1 - User
+ * */
 var LocalStrategy = require('passport-local').Strategy;
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
@@ -16,7 +16,7 @@ var GITHUB_CLIENT_SECRET = "8ecb3e3a382f0d911a9fd624e5f5f2436b922ffd";
 
 var VKONTAKTE_APP_ID = "4875977";
 var VKONTAKTE_APP_SECRET = "FG5gyYmwD2NHyY5k9UX0";
-
+var re = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
 module.exports = function(passport) {
     passport.serializeUser(function(user, done) {
         done(null, user.id);
@@ -29,27 +29,29 @@ module.exports = function(passport) {
     });
 
     passport.use('local-signup', new LocalStrategy({
-        usernameField: 'email',
-        passwordField: 'password',
-        passReqToCallback: true
-    }, function(req, email,  password, done){
+            //firstnameField: 'firstname',
+            //surnameField: 'surname',
+            usernameField: 'email',
+            passwordField: 'password',
+            passReqToCallback: true
+        }, function(req ,email,  password, done){
             User.findOne({'local.email': email}, function(err, user) {
                 if(err){
                     return done(err);
                 }
 
+                if(re.test(email)==false) {
+                    return done(null, false, {message: 'email is invalid'});
+                }
                 if(user){
                     return done(null, false, {message: 'email is taken'})
                 } else {
                     var newUser = new User();
-                    newUser.local.surname = surname;
-                    newUser.local.name = name;
                     newUser.local.email = email;
                     newUser.local.password = password;
-
+                    //newUser.local.firstname = firstname;
+                    //newUser.local.surname = surname;
                     var commonProfile = {
-                        surname:null,
-                        name:null,
                         username: null,
                         email: email,
                         photo: null
@@ -72,11 +74,12 @@ module.exports = function(passport) {
     ));
 
     passport.use('local-login', new LocalStrategy({
+
             usernameField : 'email',
             passwordField : 'password',
             passReqToCallback : true
         },
-        function(req, email, password, done) {
+        function(req, email, password,done) {
             User.findOne({ 'local.email' :  email }, function(err, user) {
                 if (err) {
                     return done(err);
